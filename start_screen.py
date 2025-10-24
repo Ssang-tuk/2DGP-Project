2fa47602d923b4872dd2612d43e814a318ee2b6d
Ssang-tuk <goo3735@tukorea.ac.kr>
2025-10-24 22:11:26 +0900

    시작화면의 배경들 각각 그리기. run 와일문넣기

--- start_screen.py
+++ start_screen.py
@@ -24,14 +24,42 @@ class StartScreen:
 
 
     def update(self):
-        pass
+        self.blink_timer += 1
 
     def draw(self):
         clear_canvas()
+        self.background2.clip_draw(10, 138, 380, 380, 200, 400, 400, 900)
+        self.background2.clip_draw(10, 138, 380, 380, 600, 400, 400, 900)
+        self.background2.clip_draw(10, 138, 380, 380, 1000, 400, 400, 900)
 
+        self.background.clip_draw(400, 0, 385, 528, 200, 375, 400, 900)
+        self.background.clip_composite_draw(400, 0, 385, 528, 0, 'h', 600, 375, 400, 900)
+        self.background.clip_draw(400, 0, 385, 528, 1000, 375, 400, 900)
+
+        self.background.clip_draw(10, 10, 380, 100, 200, 50, 400, 100)
+        self.background.clip_draw(10, 10, 380, 100, 600, 50, 400, 100)
+        self.background.clip_draw(10, 10, 380, 100, 1000, 50, 400, 100)
+
+        self.background.clip_draw(800, 423, 270, 100, 200, 660, 155, 120)
+        self.background.clip_draw(785, 280, 290, 130, 600, 660, 155, 120)
+
+        self.background.clip_draw(785, 15, 120, 130, 955, 660, 70, 120)
+        self.background.clip_draw(845, 175, 230, 100, 1010, 650, 140, 100)
+
+        self.logo_sheet.clip_draw(
+            self.logo_x, self.logo_y, self.logo_w, self.logo_h,
+            600, 500, 500, 250
+        )
+
+        if (self.blink_timer // 30) % 2 == 0:
+            self.font.clip_draw(420, 200, 296, 224, 640, 250, 300, 100)
 
         update_canvas()
 
     def run(self):
-
+        while self.running:
+            self.handle_events()
+            self.update()
+            self.draw()
+            delay(0.03)
         close_canvas()
