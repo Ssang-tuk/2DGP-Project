7c0ca916431ed215ed804b8ca3fd27b776c2eb5e
Ssang-tuk <goo3735@tukorea.ac.kr>
2025-10-29 01:02:49 +0900

    메인 수정. select 가 들어가도록

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pico2d import *
 from start_screen import StartScreen
-
+from select_screen import SelectScreen
 
 if __name__ == "__main__":
     open_canvas(1200, 800)
@@ -8,4 +8,10 @@ if __name__ == "__main__":
     start = StartScreen()
     next_scene = start.run()
 
+    if next_scene == "SELECT":
+        select = SelectScreen()
+        select.run()
+
     close_canvas()
+
+
