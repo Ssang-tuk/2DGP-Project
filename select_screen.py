bfe93d472260e2f1519b9d13f82c5a308cbf7e94
Ssang-tuk <goo3735@tukorea.ac.kr>
2025-10-30 01:04:13 +0900

    Update select_screen.py

---
+++ select_screen.py
@@ -0,0 +1,21 @@
+from pico2d import *
+
+class SelectScreen:
+    def __init__(self):
+        pass
+
+    def handle_events(self):
+        pass
+
+
+    def update(self):
+        pass
+
+    def draw(self):
+        clear_canvas()
+        
+
+        update_canvas()
+
+    def run(self):
+        pass
