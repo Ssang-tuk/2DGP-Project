28d69c69fa4b670e0c8b3d28b220fdb558fb6945
Ssang-tuk <goo3735@tukorea.ac.kr>
2025-11-01 01:07:10 +0900

    select_draw 파트 1

--- select_screen.py
+++ select_screen.py
@@ -20,6 +20,12 @@ class SelectScreen:
     def draw(self):
         clear_canvas()
 
+        self.select_sheet.clip_draw(0, 650, 100, 100, 600, 400, 1200, 800)
+
+        self.font_sheet.clip_draw(350, 119, 130, 18, 600, 700, 600, 70)
+
+        self.select_sheet.clip_draw(0, 495, 110, 125, 500, 450, 200, 250)  # Ryu
+        self.select_sheet.clip_draw(0, 370, 110, 125, 700, 450, 200, 250)  # Ken
 
         update_canvas()
 
