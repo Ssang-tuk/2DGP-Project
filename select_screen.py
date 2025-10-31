a1f4e513105e8c25df4bd2665e6dd96ff5a51fb6
Ssang-tuk <goo3735@tukorea.ac.kr>
2025-11-01 01:06:21 +0900

    init 부분 필요한것들 채워넣기

--- select_screen.py
+++ select_screen.py
@@ -2,7 +2,13 @@ from pico2d import *
 
 class SelectScreen:
     def __init__(self):
-        pass
+        self.select_sheet = load_image('Street Fighter/Select.png')
+        self.font_sheet = load_image('Street Fighter/Font.png')
+        self.running = True
+        self.blink_timer = 0
+        self.p1_selected_index = 0
+        self.p2_selected_index = 1
+
 
     def handle_events(self):
         pass
