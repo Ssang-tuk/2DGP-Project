a098f3188939b7dd889e6687c9591da0777eb5a2
Ssang-tuk <goo3735@tukorea.ac.kr>
2025-11-05 01:45:26 +0900

    fight_screen 기본 구조 만들기

---
+++ fight_screen.py
@@ -0,0 +1,24 @@
+from pico2d import *
+
+class FightScreen:
+    def __init__(self):
+        pass
+
+    def handle_events(self):
+        events = get_events()
+
+
+    def update(self):
+        pass
+
+    def draw(self):
+        clear_canvas()
+
+
+
+
+        update_canvas()
+
+    def run(self):
+
+        close_canvas()
