cb588709321f46bcb770b9fbfa4025fbefc8f867
Ssang-tuk <goo3735@tukorea.ac.kr>
2025-11-07 02:35:35 +0900

    배경이 투명한 스테이지 이미지 추가

--- select_screen.py
+++ select_screen.py
@@ -34,13 +34,13 @@ class SelectScreen:
     def update(self):
         self.blink_timer += 1
 
-
     def draw(self):
         clear_canvas()
 
         self.select_sheet.clip_draw(0, 650, 100, 100, 600, 400, 1200, 800)
 
         self.font_sheet.clip_draw(350, 119, 130, 18, 600, 700, 600, 70)
+
 
         self.select_sheet.clip_draw(0, 495, 110, 125, 500, 450, 200, 250)  # Ryu
         self.select_sheet.clip_draw(0, 370, 110, 125, 700, 450, 200, 250)  # Ken
@@ -49,7 +49,6 @@ class SelectScreen:
             self.select_sheet.clip_draw(0, 495, 110, 125, 200, 450, 300, 350)
         else:  # Ken
             self.select_sheet.clip_draw(0, 370, 110, 125, 200, 450, 300, 350)
-
             # 2P의 현재 선택 인덱스로 오른쪽 캐릭터 표시
         if self.p2_selected_index == 0:  # Ryu
             self.select_sheet.clip_composite_draw(0, 500, 110, 125, 0, 'h', 1025, 450, 300, 350)
@@ -81,4 +80,3 @@ class SelectScreen:
             self.update()
             self.draw()
             delay(0.03)
-
