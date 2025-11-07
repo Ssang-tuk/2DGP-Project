6e96428f77f5b6ca05f85f8865422418e4b8d5d5
Ssang-tuk <goo3735@tukorea.ac.kr>
2025-11-08 02:37:30 +0900

    select -> fight 화면 전환 기능 추가

--- select_screen.py
+++ select_screen.py
@@ -1,4 +1,5 @@
 from pico2d import *
+from fight_screen import FightScreen  # ← fight_screen 불러오기
 
 class SelectScreen:
     def __init__(self):
@@ -16,12 +17,21 @@ class SelectScreen:
         for e in events:
             if e.type == SDL_QUIT:
                 self.running = False
+            elif e.type == SDL_MOUSEBUTTONDOWN:
+                # 마우스로 START 글자 누를 때 감지
+                mx, my = e.x, 800 - e.y  # pico2d 좌표 변환
+                # "PRESS START" 클릭 범위
+                if 500 < mx < 800 and 150 < my < 250:
+                    print("START 클릭됨")
+                    self.running = False
+                    self.next_scene = "FIGHT"
             elif e.type == SDL_KEYDOWN:
                 if e.key == SDLK_ESCAPE:
                     self.running = False
                 elif e.key == SDLK_RETURN:
                     print(f"1P 선택: {self.p1_selected_index}, 2P 선택: {self.p2_selected_index}")
                     self.running = False
+                    self.next_scene = "FIGHT"
                 elif e.key == SDLK_a:
                     self.p1_selected_index = (self.p1_selected_index - 1) % 2
                 elif e.key == SDLK_d:
@@ -36,36 +46,31 @@ class SelectScreen:
 
     def draw(self):
         clear_canvas()
-
+        # 배경
         self.select_sheet.clip_draw(0, 650, 100, 100, 600, 400, 1200, 800)
 
         self.font_sheet.clip_draw(350, 119, 130, 18, 600, 700, 600, 70)
+        self.select_sheet.clip_draw(0, 495, 110, 125, 500, 450, 200, 250)
+        self.select_sheet.clip_draw(0, 370, 110, 125, 700, 450, 200, 250)
 
-
-        self.select_sheet.clip_draw(0, 495, 110, 125, 500, 450, 200, 250)  # Ryu
-        self.select_sheet.clip_draw(0, 370, 110, 125, 700, 450, 200, 250)  # Ken
-
-        if self.p1_selected_index == 0:  # Ryu
+        if self.p1_selected_index == 0:
             self.select_sheet.clip_draw(0, 495, 110, 125, 200, 450, 300, 350)
-        else:  # Ken
+        else:
             self.select_sheet.clip_draw(0, 370, 110, 125, 200, 450, 300, 350)
-            # 2P의 현재 선택 인덱스로 오른쪽 캐릭터 표시
-        if self.p2_selected_index == 0:  # Ryu
+
+        if self.p2_selected_index == 0:
             self.select_sheet.clip_composite_draw(0, 500, 110, 125, 0, 'h', 1025, 450, 300, 350)
             self.select_sheet.clip_draw(0, 495, 110, 25, 1000, 300, 300, 70)
-        else:  # Ken
+        else:
             self.select_sheet.clip_composite_draw(0, 370, 110, 125, 0, 'h', 1025, 450, 300, 350)
             self.select_sheet.clip_draw(0, 370, 110, 25, 1000, 300, 300, 70)
 
+        # 선택 테두리
         if (self.blink_timer // 20) % 2 == 0:
             x1, y1 = self.character_positions[self.p1_selected_index]
             x2, y2 = self.character_positions[self.p2_selected_index]
-
-            # 사각형 테두리
             draw_rectangle(x1 - 80, y1 - 170, x1 + 110, y1 + 70, 255, 0, 0)
             draw_rectangle(x2 - 80, y2 - 170, x2 + 110, y2 + 70, 0, 0, 255)
-
-            # "1P" / "2P" 텍스트
             self.font.draw(x1 - 30, y1 + 110, "1P", (255, 0, 0))
             self.font.draw(x2 + 10, y2 + 110, "2P", (0, 0, 255))
 
@@ -75,8 +80,10 @@ class SelectScreen:
         update_canvas()
 
     def run(self):
+        self.next_scene = None
         while self.running:
             self.handle_events()
             self.update()
             self.draw()
             delay(0.03)
+        return self.next_scene
