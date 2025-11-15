9b2bb69f4f3a74adaf997e982852a3185bd7c8f6
Ssang-tuk <goo3735@tukorea.ac.kr>
2025-11-15 22:02:54 +0900

    프레임 리스트 구조 넣기 + 캐릭터에서 init 만들기

--- character.py
+++ character.py
@@ -2,8 +2,36 @@ from pico2d import *
 
 class Character:
     def __init__(self, sheet, x, y, flip=False):
+        self.sheet = sheet
+        self.x, self.y = x, y
+        self.flip = flip
 
-        pass
+        # 기본 상태
+        self.state = "IDLE"
+        self.frame = 0
+        self.ftimer = 0
+
+        # 이동 속도
+        self.vx = 0
+        self.vy = 0
+
+        # 바닥 위치
+        self.ground_y = y
+
+        # 점프 파라미터
+        self.jump_force = 14
+        self.gravity = 1
+
+        # 프레임 리스트
+        self.idle_frames = []
+        self.walk_forward_frames = []
+        self.walk_backward_frames = []
+        self.punch_frames = []
+        self.kick_frames = []
+        self.jump_frames = []
+        self.crouch_frames = []
+        self.crouch_punch_frames = []
+        self.crouch_kick_frames = []
 
     def handle_action(self, action):
 
--- ryu.py
+++ ryu.py
@@ -7,3 +7,13 @@ class Ryu(Character):
         sheet = load_image("Street Fighter/Ryu.png")
         super().__init__(sheet, x, y, flip)
 
+        self.idle_frames = []
+        self.walk_forward_frames = []
+        self.walk_backward_frames = []
+        self.punch_frames = []
+        self.kick_frames = []
+        self.jump_frames = []
+        self.crouch_frames = []
+        self.crouch_punch_frames = []
+        self.crouch_kick_frames = []
+
