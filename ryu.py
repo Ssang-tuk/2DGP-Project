6fe33675f8f94129cd4425e7b3bf8d7b0e770aae
Ssang-tuk <goo3735@tukorea.ac.kr>
2025-11-16 22:08:44 +0900

    Ryu 의 행동 스프라이트 좌표 싹다 가져오기

--- ryu.py
+++ ryu.py
@@ -7,13 +7,13 @@ class Ryu(Character):
         sheet = load_image("Street Fighter/Ryu.png")
         super().__init__(sheet, x, y, flip)
 
-        self.idle_frames = []
-        self.walk_forward_frames = []
-        self.walk_backward_frames = []
-        self.punch_frames = []
-        self.kick_frames = []
-        self.jump_frames = []
-        self.crouch_frames = []
-        self.crouch_punch_frames = []
-        self.crouch_kick_frames = []
+        self.idle_frames = [(1, 11116, 64, 96), (66, 11116, 64, 96), (131, 11116, 64, 96), (196, 11116, 64, 96)]
+        self.walk_forward_frames = [(1, 10745, 79, 95), (82, 10745, 79, 95), (163, 10745, 79, 95), (244, 10745, 79, 95), (325, 10745, 79, 95)]  # 상대쪽
+        self.walk_backward_frames = [(1, 10648, 79, 95), (82, 10648, 79, 95), (163, 10648, 79, 95), (244, 10648, 79, 95), (325, 10648, 79, 95), (406, 10648, 79, 95)]  # 뒤로가기
+        self.punch_frames = [(1, 10132, 127, 94), (130, 10132, 127, 94), (259, 10132, 127, 94)]
+        self.kick_frames = [(1, 9258, 127, 94), (130, 9258, 127, 94)]
+        self.jump_frames = [(1, 10422, 63, 111), (66, 10422, 63, 111), (131, 10422, 63, 111), (196, 10422, 63, 111), (261, 10422, 63, 111), (326, 10422, 63, 111)]
+        self.crouch_frames = [(1, 10907, 80, 95), (82, 10907, 80, 95), (163, 10907, 80, 95)]
+        self.crouch_punch_frames = [(1, 9662, 111, 62), (114, 9662, 111, 62), (227, 9662, 111, 62)]
+        self.crouch_kick_frames = [(1, 8757, 159, 62), (162, 8757, 159, 62)]
 
